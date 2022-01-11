59318eaad1038ce7f66058e4b4ebaf58ac56c093
Elisei <ezenevich99@gmail.com>
2022-01-12 00:06:27 +0300

    [FTR] loan products generated on page with json data

--- task 2 - json/scripts/getJson.js
+++ task 2 - json/scripts/getJson.js
@@ -10,8 +10,35 @@ function readJsonFile(file, callback) {
     rawFile.send(null);
 }
 
+
 //use func
 readJsonFile("./content/loanProducts.json", function(text){
-    let data = JSON.parse(text);
-    console.log(data);
+    let products = JSON.parse(text);
+    let productsList = document.getElementsByClassName("products-list").item(0);
+
+    products.forEach(prod => {
+        let productsListItem = document.createElement('div');
+        let productsListItemName = document.createElement('h3');
+        let productsListItemNDate = document.createElement('p');
+
+        productsListItem.className = "products-list-item";
+        productsListItemName.className = "products-list-item__name";
+        productsListItemNDate.className = "products-list-item__date";
+
+        productsListItemName.innerText = prod.name;
+        productsListItemName.setAttribute("id", prod.id);
+        productsListItemName.setAttribute("min-amount", prod.min_amount);
+        productsListItemName.setAttribute("max-amount", prod.max_amount);
+        productsListItemName.setAttribute("min-term", prod.min_term);
+        productsListItemName.setAttribute("max-term", prod.max_term);
+        productsListItemName.setAttribute("rate", prod.rate);
+        productsListItemNDate.innerText = prod.date;
+
+        productsListItem.appendChild(productsListItemName);
+        productsListItem.appendChild(productsListItemNDate);
+        productsList.appendChild(productsListItem);
+    })
 });
+
+
+
